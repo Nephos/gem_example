780d7ac5d44b96da2a550185db992ae3957ed43e
Arthur Poulet <arthur.poulet@mailoo.org>
2016-08-26 16:40:03 +0200

    Add modules Fetcher and Downloader
    * add module Downloader which downloads the image specified in a path
    * move the fetch_??? functions to a new module (Fetcher)
    * prepare the Fetcher module to the multi-threading

---
+++ lib/manga_fetch/downloader.rb
@@ -0,0 +1,12 @@
+module MangaFetch::Downloader
+  extend self
+
+  def download_image(src: nil, prefix: "/tmp")
+    image = @agent.get src
+    path = File.expand_path image.uri.to_s.split("/")[-3..-1].join("/"), prefix
+    Dir.mkdir File.expand_path image.uri.to_s.split("/")[-3..-3].join("/"), prefix rescue nil
+    Dir.mkdir File.expand_path image.uri.to_s.split("/")[-3..-2].join("/"), prefix rescue nil
+    File.write path, image.body
+    STDERR.puts "File [".blue + path.yellow + "] saved"
+  end
+end
---
+++ lib/manga_fetch/fetcher.rb
@@ -0,0 +1,42 @@
+module MangaFetch::Fetcher
+  extend self
+
+  # Fetch some pages form the manga named like `name`
+  def fetch(name: "berserk", num: nil)
+    init_agent("http://www.mangareader.net/#{name}")
+
+    images = if num.is_a? Fixnum
+      [ fetch_one(num) ]
+    elsif num.is_a? Enumerable
+      fetch_list(num)
+    end
+  end
+
+  def init_agent(url)
+    @agent = Mechanize.new
+    @agent.get url
+    @main = @agent.page
+  end
+
+  def fetch_one(num: 1)
+    link = "#{@main.uri.to_s}/#{num}"
+    STDERR.puts "Fetching Tome [".blue + "#{num}".yellow + "]".blue if $verbose
+    first_page = @agent.get(link)
+
+    last_page_num = first_page.at("#selectpage").text.split.last.to_i
+    (1..last_page_num).map do |i|
+      STDERR.puts "Fetching Tome [".blue + "#{num}".yellow + "] Image [".blue + "#{i} / #{last_page_num}".yellow + "]".blue if $verbose
+      current_page = @agent.get "#{first_page.uri.to_s}/#{i}"
+      image = current_page.at("#img")[:src]
+      STDERR.puts "-> ".blue + image.red if $verbose
+      image
+    end
+
+  end
+
+  def fetch_list(nums)
+    ones = []
+    nums.map{|num| ones << fetch_one(num: 1) } #  , threads: 4
+    ones
+  end
+end
